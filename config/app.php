c3266c070bcce858dd20e7b10eaa5f80ac27f310
Joost Tiemstra <joost@tjuna.com>
2017-06-14 15:47:03 +0200

    Composer update

--- config/app.php
+++ config/app.php
@@ -179,6 +179,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        'Maatwebsite\Excel\ExcelServiceProvider',
+
     ],
 
     /*
@@ -227,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Excel' => 'Maatwebsite\Excel\Facades\Excel',
 
     ],
 
